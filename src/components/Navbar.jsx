a5a1cfd61f7ec63ce64cf70c80844d7fba82cb7e
Will-Marco <mr.marco0101@gmail.com>
2023-09-22 07:55:31 +0500

    edit navbar class attr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { logo } from "../constants";
 
 const Navbar = () => {
   return (
-    <div class='d-flex flex-column flex-md-row align-items-center py-3 mb-4 border-bottom container'>
+    <div className='d-flex flex-column flex-md-row align-items-center py-3 mb-4 border-bottom container'>
 			<Link to={'/'}>
 				<img src={logo} alt='logo' height={'40px'}/>
 			</Link>
 
-			<nav class='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
+			<nav className='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
 				<Link className='me-3 py-2 text-dark text-decoration-none' to={'/login'}>
 					Login
 				</Link>
