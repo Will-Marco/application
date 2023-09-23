ea079ba3ef005ca8c033f7145ec91868498e2b8c
Will-Marco <mr.marco0101@gmail.com>
2023-09-23 21:44:16 +0500

    add redux register slice

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import { graduationCapIcon } from "../constants";
 import { Input } from "../ui";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUserStart } from "../slice/auth";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector((state) => state.auth);
+
+  const registerHandler = (e) => {
+    e.preventDefault();
+    dispatch(registerUserStart());
+  };
 
   return (
     <div className="text-center mt-5">
@@ -14,11 +23,7 @@ const Register = () => {
           <img className="mb-4" src={graduationCapIcon} alt="" width="72" />
           <h1 class="h3 mb-3 fw-normal">Please register</h1>
 
-          <Input
-            label={"Username"}
-            state={name}
-            setState={setName}
-          />
+          <Input label={"Username"} state={name} setState={setName} />
           <Input
             label={"Email address"}
             type={"email"}
@@ -32,8 +37,13 @@ const Register = () => {
             setState={setPassword}
           />
 
-          <button className="btn btn-primary w-100 mt-4 py-2" type="submit">
-          Register
+          <button
+            className="btn btn-primary w-100 mt-4 py-2"
+            type="submit"
+            disabled={isLoading}
+            onClick={registerHandler}
+          >
+            {isLoading ? "Please wait..." : "Register"}
           </button>
         </form>
       </main>
