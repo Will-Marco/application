264837d284ea8c16e637c52d109731190f495ec7
Will-Marco <mr.marco0101@gmail.com>
2023-09-30 20:47:22 +0500

    add auth navigate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { ValidationError } from "./";
 import { graduationCapIcon } from "../constants";
@@ -10,7 +11,8 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
-  const { isLoading } = useSelector((state) => state.auth);
+  const { isLoading, loggedIn } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
 
   const loginHandler = async (e) => {
     e.preventDefault();
@@ -19,10 +21,17 @@ const Login = () => {
     try {
       const response = await AuthService.userLogin(user);
       dispatch(signUserSuccess(response.user));
+      navigate("/");
     } catch (error) {
       dispatch(signUserFailure(error.response.data.errors));
     }
   };
+
+  useEffect(() => {
+    if (loggedIn) {
+      navigate("/");
+    }
+  }, [loggedIn, navigate]);
 
   return (
     <div className="text-center mt-5">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,41 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { logo } from "../constants";
 
 const Navbar = () => {
-  return (
-    <div className='d-flex flex-column flex-md-row align-items-center py-3 mb-4 border-bottom container'>
-			<Link to={'/'}>
-				<img src={logo} alt='logo' height={'40px'}/>
-			</Link>
+  const { loggedIn, user } = useSelector((state) => state.auth);
 
-			<nav className='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
-				<Link className='me-3 py-2 text-dark text-decoration-none' to={'/login'}>
-					Login
-				</Link>
-				<Link className='me-3 py-2 text-dark text-decoration-none' to={'/register'}>
-					Register
-				</Link>
-			</nav>
-		</div>
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center py-3 mb-4 border-bottom container">
+      <Link to={"/"}>
+        <img src={logo} alt="logo" height={"40px"} />
+      </Link>
+      <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        {loggedIn ? (
+          <>
+            <p className="me-3 m-0 py-2 text-dark text-decoration-none">
+              {user.username}
+            </p>
+            <button className="btn btn-outline-danger">Log Out</button>
+          </>
+        ) : (
+          <>
+            <Link
+              className="me-3 py-2 text-dark text-decoration-none"
+              to={"/login"}
+            >
+              Login
+            </Link>
+            <Link
+              className="me-3 py-2 text-dark text-decoration-none"
+              to={"/register"}
+            >
+              Register
+            </Link>
+          </>
+        )}
+      </nav>
+    </div>
   );
 };
 export default Navbar;
