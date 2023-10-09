3021d822edd746ba5bfcc34030d16b9601974997
Will-Marco <mr.marco0101@gmail.com>
2023-10-09 22:45:25 +0500

    change API URL

--- src/service/api.js
+++ src/service/api.js
@@ -1,13 +1,13 @@
-import axios from 'axios'
-import { getItem } from '../helpers/persistence-storange'
+import axios from "axios";
+import { getItem } from "../helpers/persistence-storange";
 
-axios.defaults.baseURL = "http://localhost:3000/api"
+axios.defaults.baseURL = "https://api.realworld.io/api";
 
-axios.interceptors.request.use(config => {
-    const token = getItem('token')
-    const authorization = token ? `Token ${token}` : ''
-    config.headers.Authorization = authorization
-    return config
-})
-    
-export default axios
+axios.interceptors.request.use((config) => {
+  const token = getItem("token");
+  const authorization = token ? `Token ${token}` : "";
+  config.headers.Authorization = authorization;
+  return config;
+});
+
+export default axios;
